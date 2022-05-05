d6cdcdef6228feb4992814a01795ad55dfdf2fb3
shreyanspatel19 <200540107107@darshan.ac.in>
2022-05-05 21:46:57 +0530

    m

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn shreyans React
         </a>
       </header>
     </div>
