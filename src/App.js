424b988bf43458b1ea057eab40b6ceda8054b834
shreyanspatel19 <200540107107@darshan.ac.in>
2022-05-07 11:41:18 +0530

    s

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   <div className="container-fluid">
     <a className="navbar-brand" href="/">React</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon"> </span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -28,7 +28,7 @@ function App() {
     </div>
   </div>
 </nav>
- <div className='black'><h1>
+ <div className='black container'><h1>
 shreyans
 </h1>
  </div>
